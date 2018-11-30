7f59291fb1a2f66ad1713604dd1dcbdf73370f34
g.babikov <g.babikov@corp.vk.com>
2018-11-30 08:05:08 +0700

    Ебнул кофе

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var rjomba_idet = "net";
+var roflan;
 
 var Sanya = {
     init_rjomba: function(d) {
         if (rjomba_idet == "net") {
             var rofl = d.getAttribute("data-rjaka");
-            document.getElementById("sanya_rjomba").innerHTML = "<audio id='rjomba' src='" + rofl + ".mp3' autoplay loop></audio>";
+            Sanya.start_rjomba(rofl,true);
             rjomba_idet = "da";
         } else {
-            document.getElementById("sanya_rjomba").innerHTML = "";
+            roflan.pause();
             rjomba_idet = "net"
         }
     },
+    start_rjomba: function(rofl,loop){
+      roflan = new Audio(rofl+".mp3");
+      roflan.loop = loop;
+      roflan.autoplay = true;
+      roflan.id = "kek";
+    }
 };
+
+//Я ебал
